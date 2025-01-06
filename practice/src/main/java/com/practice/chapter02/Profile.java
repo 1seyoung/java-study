befeb357d60bbe8fbdcf4adf411060686cc7ea27
1seyoung <20171631@edu.hanbat.ac.kr>
2025-01-06 18:36:16 +0900

    chapter1 - 5 실습 코드

---
+++ practice/src/main/java/com/practice/chapter02/Profile.java
@@ -0,0 +1,10 @@
+package com.practice.chapter02;
+
+public class Profile {
+    public static void main(String[] args) {
+        String name = "000";
+        int age = 0;
+        System.out.println("My name is "+name);
+        System.out.println("My age is " + age);
+    }
+}
