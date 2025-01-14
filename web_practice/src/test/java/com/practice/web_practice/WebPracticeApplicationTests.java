0267ef7f17d0a74d8847a58931be2053825c4355
1seyoung <akdlrnal0705@gmail.com>
2025-01-14 15:59:07 +0900

    인프런 강의 실습

---
+++ web_practice/src/test/java/com/practice/web_practice/WebPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.web_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
